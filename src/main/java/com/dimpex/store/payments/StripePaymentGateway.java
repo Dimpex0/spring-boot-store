61dd0b29ad2f735b855426a014e588f4baa3f56f
Dimpex0 <mitkodd2005@gmail.com>
2025-07-26 18:17:49 +0300

    fixed payment metadata

--- src/main/java/com/dimpex/store/payments/StripePaymentGateway.java
+++ src/main/java/com/dimpex/store/payments/StripePaymentGateway.java
@@ -36,7 +36,8 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntent(order)
+                    );
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -50,6 +51,12 @@ public class StripePaymentGateway implements PaymentGateway {
             System.out.println(ex.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private static SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
+                .build();
     }
 
     @Override
